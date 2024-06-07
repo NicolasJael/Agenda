d66d50be885c0ab4992f00a9987592748dd59c56
DEV <DEV@DEV03>
2024-06-07 16:21:34 -0300

    Segundo commit

--- Agenda/src/com/nicolas/agenda/gerenciador/GerenciadorCont.java
+++ Agenda/src/com/nicolas/agenda/gerenciador/GerenciadorCont.java
@@ -1,110 +1,45 @@
 package com.nicolas.agenda.gerenciador;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 import com.nicolas.agenda.domain.Contato;
 
 public class GerenciadorCont {
 
-	private static Scanner scanner;
 	private static List<Contato> contatos = new ArrayList<>();
 	private static int codigo = 1;
 
-	public GerenciadorCont(Scanner scanner) {
-		
-		GerenciadorCont.scanner = scanner;
-		inicializar();
-	}
+	
 
-	private void inicializar() {
-		
-		contatos.add(new Contato("Nicolas", getCod(), "99988445621"));
-		contatos.add(new Contato("Felipe", getCod(), "9984457132"));
-		contatos.add(new Contato("Dyogo", getCod(), "99995858"));
-	}
+	public static void add(Contato contato) {
 
-	private static int getCod() {
-		
-		return codigo++;
-	}
-
-	public void listarContatos() {
-		
-		System.out.println("Os contatos adicionados foram:");
-		for (Contato contato : contatos) {
-			System.out.println(" - Cod:  " + contato.getCodigo() + " - Nome: " + contato.getNome() + " - Telefone: "
-					+ contato.getTelefone());
-		}
-	}
-
-	public void criaContato() {
-		
-		Contato contato = new Contato();
-		System.out.print("Qual o nome do contato? ");
-		String nomeCont = scanner.nextLine();
-		System.out.print("Qual é o número de telefone do contato? ");
-		String numCont = scanner.nextLine();
-
-		contato.setNome(nomeCont);
-		contato.setTelefone(numCont);
-		contato.setCodigo(getCod());
+		contato.setCodigo(codigo++);
 		contatos.add(contato);
 	}
 
-	public void alterarContato() {
+	public static void lerArq() {
 		
-		listarContatos();
-		System.out.print("Qual é o código do contato a ser alterado?");
-		int alterarCont = Integer.parseInt(scanner.nextLine());
-
-		Contato contato = null;
-		for (Contato c : contatos) {
-			if (c.getCodigo() == alterarCont) {
-				contato = c;
-				break;
-			}
+		String dados;
+		try {
+			dados = new String(Files.readAllBytes(new File("C:\\Users\\DEV\\Documents\\arq_agenda\\Lista_agenda.csv").toPath()));
+			System.out.println(dados);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		if (contato != null) {
-			System.out.print("Digite o novo nome do contato:");
-			contato.setNome(scanner.nextLine());
-			System.out.print("Digite o novo número de telefone:");
-			contato.setTelefone(scanner.nextLine());
-			System.out.println("Contato alterado com sucesso!");
-		} else {
-			System.out.println("Contato não encontrado :(");
-		}
+		
 	}
+		
 
-	public void apagaContato() {
-
-		listarContatos();
-		System.out.print("Digite o código a ser apagado:");
-		int apagarCont = Integer.parseInt(scanner.nextLine());
-
-		Contato contato = null;
-		for (Contato c : contatos) {
-			if (c.getCodigo() == apagarCont) {
-				contato = c;
-				break;
-			}
-		}
-		if (contato != null) {
-			contatos.remove(contato);
-			System.out.println("Contato apagado com sucesso!");
-		} else {
-			System.out.println("Contato não encontrado :(");
-		}
-	}
-
-	public void gravar() throws IOException {
+	
+	public static void gravar() throws IOException {
 
 		FileWriter arq = new FileWriter("C:\\Users\\DEV\\Documents\\arq_agenda\\Lista_agenda.csv");
-		listarContatos();
 		try (PrintWriter gravarArq = new PrintWriter(arq)) {
 			gravarArq.printf("codigo;nome;Telefone%n");
 			for (Contato contato : contatos) {
@@ -114,4 +49,30 @@ public class GerenciadorCont {
 		arq.close();
 		System.out.println("Salvo com sucesso!");
 	}
+
+	public static List<Contato> getContatos() {
+
+		return contatos;
+	}
+
+	public static Contato getContato(int codigo) {
+
+		for (Contato contato : contatos) {
+			if (contato.getCodigo() == codigo) {
+				return contato;
+			}
+		}
+
+		return null;
+	}
+
+	public static void apagar(Contato contato) {
+
+		contatos.remove(codigo);
+	}
+
+	public static void alterar(Contato contato) {
+		
+	}
+
 }
--- Agenda/src/com/nicolas/agenda/ui/Menu.java
+++ Agenda/src/com/nicolas/agenda/ui/Menu.java
@@ -1,23 +1,109 @@
 package com.nicolas.agenda.ui;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.Scanner;
+
+import com.nicolas.agenda.domain.Contato;
+import com.nicolas.agenda.gerenciador.GerenciadorCont;
 
 public class Menu {
 
-	public static int menu(Scanner scanner) {
+	private Scanner scanner = new Scanner(System.in);
 
-		System.out.println("---------------------");
-		System.out.println("1 - Listar");
-		System.out.println("2 - Incluir");
-		System.out.println("3 - Alterar");
-		System.out.println("4 - Apagar");
-		System.out.println("5 - Salvar");
-		System.out.println("6 - Sair");
-		System.out.print("Escolha uma opção: ");
+	public void exibir() throws IOException {
 
-		int opcao = scanner.nextInt();
-		scanner.nextLine();
-		return opcao;
+		for (;;) {
+			System.out.println("---------------------");
+			System.out.println("1 - Exibir lista antiga");
+			System.out.println("2 - Listar");
+			System.out.println("3 - Incluir");
+			System.out.println("4 - Alterar");
+			System.out.println("5 - Apagar");
+			System.out.println("6 - Salvar");
+			System.out.println("7 - Sair");
+			System.out.print("Escolha uma opção: ");
+
+			int opcao = scanner.nextInt();
+			scanner.nextLine();
+			switch (opcao) {
+			case 1:
+				GerenciadorCont.lerArq();
+				break;
+			case 2:
+				listarContatos();
+				break;
+			case 3:
+				criaContato();
+				break;
+			case 4:
+				alterarContato();
+				break;
+			case 5:
+				apagaContato();
+				break;
+			case 6:
+				GerenciadorCont.gravar();
+				break;
+			case 7:
+				scanner.close();
+				System.exit(0);
+			default:
+				System.out.println("Opção inválida");
+			}
+		}
 	}
 
+	public void listarContatos() {
+
+		List<Contato> contatos = GerenciadorCont.getContatos();
+
+		System.out.println("Os contatos adicionados foram:");
+		for (Contato contato : contatos) {
+			System.out.println(" - Cod:  " + contato.getCodigo() + " - Nome: " + contato.getNome() + " - Telefone: "
+					+ contato.getTelefone());
+		}
+	}
+
+	public void criaContato() {
+
+		System.out.print("Qual o nome do contato? ");
+		String nomeCont = scanner.nextLine();
+		System.out.print("Qual é o número de telefone do contato? ");
+		String numCont = scanner.nextLine();
+
+		Contato contato = new Contato();
+		contato.setNome(nomeCont);
+		contato.setTelefone(numCont);
+		GerenciadorCont.add(contato);
+	}
+
+	public void alterarContato() {
+
+		listarContatos();
+		System.out.print("Qual é o código do contato a ser alterado: ");
+		int codigo = Integer.parseInt(scanner.nextLine());
+		Contato contato = GerenciadorCont.getContato(codigo);
+
+		if (contato != null) {
+			System.out.print("Digite o novo nome do contato:");
+			contato.setNome(scanner.nextLine());
+			System.out.print("Digite o novo número de telefone:");
+			contato.setTelefone(scanner.nextLine());
+			System.out.println("Contato alterado com sucesso!");
+		} else {
+			System.out.println("codigo nao encontrado");
+		}
+
+		GerenciadorCont.alterar(contato);
+	}
+
+	public void apagaContato() {
+
+		listarContatos();
+		System.out.print("Digite o código a ser apagado: ");
+		int codigo = Integer.parseInt(scanner.nextLine());
+		Contato contato = GerenciadorCont.getContato(codigo);
+		GerenciadorCont.apagar(contato);
+	}
 }
